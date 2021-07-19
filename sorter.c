56acdb92eb474138ebbedb8a77097f057b66f2ad
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2021-07-19 23:44:57 +0100

    Add compare function to make buble sort more generic

--- sorter.c
+++ sorter.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
-void bubbleSort(int A[], int n) {
+int compare(int a, int b) {
+    return a > b ? 1: -1;
+}
+
+/*
+  Callback function should compare two integers, should return 1 if first element has
+  higher rank, 0 if elements are equal and - 1 if second element has higher rank
+ */
+void bubbleSort(int A[], int n, int(*compare)(int, int)) {
     int temp;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n - 1; j++) {
-            if (A[i] > A[j]) {
+            if (compare(A[i], A[j]) < 0) { 
                 temp = A[i];
                 A[i] = A[j];
                 A[j] = temp;
@@ -16,6 +24,6 @@ void bubbleSort(int A[], int n) {
 int main() {
     int A[] = {3,2,1,5,6,4};
     int size = sizeof(A) / sizeof(int);
-    bubbleSort(A, size);
+    bubbleSort(A, size, compare);
     for(int i = 0; i < size; i++) printf("%d ", A[i]);
 }
